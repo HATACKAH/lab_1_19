809e3df876ef65f8424b5793241f8c96137c8977
HATACKAH <66951536+HATACKAH@users.noreply.github.com>
2020-06-15 19:37:15 +0400

    Update text.cpp

--- text.cpp
+++ text.cpp
@@ -4,18 +4,17 @@
 using namespace std;
 
 int main() {
-
+int a;
+a=0;
 ifstream text;
 text.open("laba_1.txt");
-    
-
     string word;
     while (text >> word)
     {
-       cout << word << ".";
+       a=a+1;
     }
 
-    
+    cout <<a<< endl;
     return 0;
 
 }
