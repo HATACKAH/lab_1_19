d6cd7e4a9c38ce8e3587ff3c0a6d3a0605878290
HATACKAH <66951536+HATACKAH@users.noreply.github.com>
2020-06-15 19:59:17 +0400

    Update text.cpp

--- text.cpp
+++ text.cpp
@@ -3,22 +3,21 @@
 
 using namespace std;
 
-int main() {
-int a;
-a=0;
-ifstream input; 
-ofstream output;
-input.open("input.txt");
-output.open("output.txt");
+int main()
+{
+    int a;
+    a = 0;
+    ifstream input;
+    ofstream output;
+    input.open("input.txt");
+    output.open("output.txt");
     string word;
     while (input >> word)
     {
-       a=a+1;
+        a = a + 1;
     }
 
     output << a;
     output.close();
     return 0;
-
-
 }
